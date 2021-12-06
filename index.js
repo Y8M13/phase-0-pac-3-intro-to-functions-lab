87e518682a0348b7e3507a7c6a9a834bbe9eee1b
Yolanda Morris <y.mijoe7145@gmail.com>
2021-12-06 00:45:30 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+
+function whisper(string) {
+    return string.toLowerCase();
+}
+
+function sayHiToGrandma(string){
+    console.log(string.toLowerCase())
+    return ("I can\'t hear you!");
+}
+
+function logShout(string){
+      console.log("HELLO");
+}
+
+function logWhisper(string){
+    console.log("hello");
+}
